877bb0edcdc58d8edf04223a7eaf65d62623c09b
Akash <jaimin@webbrainstechnologies.com>
2023-03-24 19:26:33 +0530

    variabel added to env file

--- src/redux/property/services.js
+++ src/redux/property/services.js
@@ -1,24 +1,23 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3005'
 const PropertyServices = {
 
     GET_STATES: async () => {
-        const response = await axios.get(`${BASE_URL}/property/get_states`)
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/property/get_states`)
         return response;
     },
 
     ADD_TS_DATA: async (data) => {
-        const response = await axios.post(`${BASE_URL}/property/add_all_ts_data`, data)
+        const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/property/add_all_ts_data`, data)
         return response
     },
 
     GET_ALL_TS_DATA: async () => {
-        const response = await axios.get(`${BASE_URL}/property/get_all_ts_data`)
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/property/get_all_ts_data`)
         return response
     },
     GET_TS_DATA_BY_RERA_NUMBER_OR_PA_ID: async (id) => {
-        const response = await axios.get(`${BASE_URL}/property/get_by_rera_number_or_pa_id?num=${id}`)
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/property/get_by_rera_number_or_pa_id?num=${id}`)
         return response
     }
 
